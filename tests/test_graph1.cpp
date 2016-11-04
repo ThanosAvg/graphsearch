c64954afa6bd1d7053b80ea6608054bdd5ba3ac0
ThanosAvg <dmtavg@gmail.com>
2016-11-04 03:46:35 +0200

    Graph test + parsing for workload files

--- tests/test_graph1.cpp
+++ tests/test_graph1.cpp
@@ -23,7 +23,7 @@ bool testLoadFile(){
     uint32_t from = 0, to = 0;
     char status = 0;
     int count = 0;
-    FILE *file = fopen("smallGraph.txt", "r");
+    FILE *file = fopen("small/smallGraph.txt", "r");
     if(file == NULL){
         std::cout << "File error" << std::endl;
         return false;
@@ -33,10 +33,46 @@ bool testLoadFile(){
         sscanf(line, "%c", &status);
         if(status != 'S'){
             sscanf(line, "%u %u", &from, &to);
-            std::cout << "(" << from << "," << to << ")" << std::endl;
+            //std::cout << "(" << from << "," << to << ")" << std::endl;
             graph.add(from, to);
         }
     }
+
+    fclose(file);
+
+    file = fopen("small/smallWorkload_FINAL.txt", "r");
+    if(file == NULL){
+        std::cout << "File error" << std::endl;
+        return false;
+    }
+
+    while((read = getline(&line, &len, file)) != -1){
+        //std::cout << "line:" << line << std::endl;
+        sscanf(line, "%c", &status);
+        if(status == 'A'){
+            sscanf(line, "%*c %u %u", &from, &to);
+            //std::cout << "addition:" << "(" << from << "," << to << ")" << std::endl;
+            graph.add(from, to);
+        }
+        else if(status == 'Q'){
+            sscanf(line, "%*c %u %u", &from, &to);
+            //std::cout << "query:" << "(" << from << "," << to << ")" << std::endl;
+            std::cout << "result:" << graph.query(from, to) << std::endl;
+        }
+        else if(status == 'F'){
+            std::cout << "RIPH"  << std::endl;
+        }
+        else if(status == 'S'){
+            std::cout << "End of file,quitting..."  << std::endl;
+            break;
+        }
+        else{
+            std::cout << "Uknown parameter,quitting..."  << std::endl;
+            break;
+        }
+    }
+
+    fclose(file);
 
     return true;
 }
